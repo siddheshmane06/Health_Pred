b7348999e416b50308c0b2b95dd12a979a2d6cb1
Siddhesh Mane <77160284+siddheshmane06@users.noreply.github.com>
2024-04-19 16:09:16 +0530

    Update 🥼_Home.py

--- 🥼_Home.py
+++ 🥼_Home.py
@@ -8,19 +8,19 @@ def home():
     st.image("img/logo.png")
     
     st.markdown(
-        '<p style="font-size:22px; text-align: center; color: black;font-size: 25px;">Connected Health, Connected Lives: Seamless Solutions for Every Step of the Journey</p>',
+        '<p style="font-size:22px; text-align: center; color: black;font-size: 25px;">Connected Health, Connected Lives: Seamless Solutions for Every Step of the Journey.</p>',
         unsafe_allow_html=True
     )
     st.markdown("---")
 
     st.markdown(
-        f"<p style='text-align: center; color: black; font-size: 20px'>Unlocking the Potential of Digital Health: Harnessing Technology, Personalized Solutions, and Advanced Analytics to Revolutionize Healthcare and Elevate Your Care Experience Today."</p>",
+        f"<p style='text-align: center; color: black; font-size: 20px'>Unlocking the Potential of Digital Health: Harnessing Technology, Personalized Solutions, and Advanced Analytics to Revolutionize Healthcare and Elevate Your Care Experience Today.</p>",
         unsafe_allow_html=True
     )
     st.markdown("---")
 
     st.markdown(
-        f"<p style='text-align: center; color: black; font-size: 20px'>💠 We provide digital health and healthcare solutions to help common people and health organizations power their care experience and improve health outcomes with advanced analytics</p>",
+        f"<p style='text-align: center; color: black; font-size: 20px'>💠 We provide digital health and healthcare solutions to help common people and health organizations power their care experience and improve health outcomes with advanced analytics.</p>",
         unsafe_allow_html=True,
     )
 
@@ -33,18 +33,18 @@ def home():
     st.write("")
 
     st.markdown(
-        f"⚕️ **Disease Diagnosis** - Enter the symptoms you are suffering from and you will get to know the disease you are suffering from, precautions to take, medications and specialists near you to cure the disease"
+        f"⚕️ **Disease Diagnosis** - Enter the symptoms you are suffering from and you will get to know the disease you are suffering from, precautions to take, medications and specialists near you to cure the disease."
     )
     st.markdown(
-        f"⚕️ **Early Diabetes Detection** - Enter the patients attributes from the test report and check whether he/she have chances of diabetes or not"
-    )
-
-    st.markdown(
-        f"⚕️ **Liver Disease Detection** - Enter the patients attributes from the test report and check whether he/she have chances of any type of liver disease or not"
+        f"⚕️ **Early Diabetes Detection** - Enter the patients attributes from the test report and check whether he/she have chances of diabetes or not."
     )
 
     st.markdown(
-        f"⚕️ **Pneumonia Detection** - Upload the chest X-ray image of the patient and check whether the patient have chances of Pneumonia"
+        f"⚕️ **Liver Disease Detection** - Enter the patients attributes from the test report and check whether he/she have chances of any type of liver disease or not."
+    )
+
+    st.markdown(
+        f"⚕️ **Pneumonia Detection** - Upload the chest X-ray image of the patient and check whether the patient have chances of Pneumonia."
     )
 
 home()  # Call the function to execute the code
